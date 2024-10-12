b282ee9d5e7f6ff5c2c2387ed1ae406a1eab645b
Veli <veli.omn@gmail.com>
2024-10-12 18:06:57 +0200

    fix: corrected compareArrayBuffers fn

--- generic/compare-array-buffers.ts
+++ generic/compare-array-buffers.ts
@@ -2,11 +2,17 @@ export function compareArrayBuffers(buffer0: ArrayBuffer, buffer1: ArrayBuffer):
     const array0: Uint8Array = new Uint8Array(buffer0);
     const array1: Uint8Array = new Uint8Array(buffer1);
 
-    for (let i = 0; i < Math.min(array0.length, array1.length); i++) {
+    // First, compare by length.
+    if (array0.length !== array1.length) {
+        return array0.length - array1.length;
+    }
+
+    // If lengths are the same, compare lexicographically.
+    for (let i = 0; i < array0.length; i++) {
         if (array0[i] !== array1[i]) {
             return array0[i] - array1[i];
         }
     }
 
-    return array0.length - array1.length;
+    return 0;
 }
