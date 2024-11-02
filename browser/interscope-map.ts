959615511bc9d1761e7f19b0a4c11c78141edd73
Veli <veli.omn@gmail.com>
2024-11-02 18:00:09 +0100

    refactor: interscope map

--- browser/interscope-map.ts
+++ browser/interscope-map.ts
@@ -1,3 +1,6 @@
+const storeName: string = "entries";
+let interscopeMapDB: IDBDatabase;
+
 function wrapRequest<T>(request: IDBRequest<T>): Promise<T> {
     return new Promise<T>((resolve, reject) => {
         request.onsuccess = () => resolve(request.result);
@@ -5,20 +8,20 @@ function wrapRequest<T>(request: IDBRequest<T>): Promise<T> {
     });
 }
 
-const openRequest: IDBOpenDBRequest = indexedDB.open("<interscope-map>");
-const storeName: string = "entries";
-let interscopeMapDB: IDBDatabase;
+async function initDB() {
+    const openRequest: IDBOpenDBRequest = indexedDB.open("<interscope-map>");
+    openRequest.onupgradeneeded = () => openRequest.result.createObjectStore(storeName);
+    interscopeMapDB = await wrapRequest(openRequest);
+}
 
-openRequest.onupgradeneeded = () => openRequest.result.createObjectStore(storeName);
-wrapRequest(openRequest).then((db) => interscopeMapDB = db);
-
-function getStore(mode: IDBTransactionMode = "readwrite"): IDBObjectStore {
+async function getStore(mode: IDBTransactionMode = "readwrite"): Promise<IDBObjectStore> {
+    if (!interscopeMapDB) await initDB();
     return interscopeMapDB.transaction(storeName, mode).objectStore(storeName);
 }
 
 export const InterscopeMap = {
-    set: (key: IDBValidKey, value: unknown): Promise<IDBValidKey> => wrapRequest(getStore().put(value, key)),
-    get: (key: IDBValidKey): Promise<unknown> => wrapRequest(getStore("readonly").get(key)),
-    delete: (key: IDBValidKey): Promise<undefined> => wrapRequest(getStore().delete(key)),
-    clear: (): Promise<undefined> => wrapRequest(getStore().clear())
+    set: async (key: IDBValidKey, value: unknown): Promise<IDBValidKey> => wrapRequest((await getStore()).put(value, key)),
+    get: async (key: IDBValidKey): Promise<unknown> => wrapRequest((await getStore("readonly")).get(key)),
+    delete: async (key: IDBValidKey): Promise<undefined> => wrapRequest((await getStore()).delete(key)),
+    clear: async (): Promise<undefined> => wrapRequest((await getStore()).clear())
 };
