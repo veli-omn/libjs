dec9e28e6cb8bb50a3058eb3b57ccbc839881997
Veli <veli.omn@gmail.com>
2025-07-09 23:49:07 +0200

    refactor: interscope map module

--- browser/interscope-map.ts
+++ browser/interscope-map.ts
@@ -1,27 +1,68 @@
-const storeName: string = "entries";
-let interscopeMapDB: IDBDatabase;
+const DB_NAME = "<im>";
+const STORE_NAME = "index";
+let databasePromise: Promise<IDBDatabase> | null = null;
 
-function wrapRequest<T>(request: IDBRequest<T>): Promise<T> {
-    return new Promise<T>((resolve, reject) => {
+function promisifyRequest<T>(request: IDBRequest<T>): Promise<T> {
+    return new Promise((resolve, reject) => {
         request.onsuccess = () => resolve(request.result);
-        request.onerror = () => reject(request.error);
+        request.onerror   = () => reject(request.error);
     });
 }
 
-async function initDB() {
-    const openRequest: IDBOpenDBRequest = indexedDB.open("<interscope-map>");
-    openRequest.onupgradeneeded = () => openRequest.result.createObjectStore(storeName);
-    interscopeMapDB = await wrapRequest(openRequest);
+function openDatabase(): Promise<IDBDatabase> {
+    if (!databasePromise) {
+        databasePromise = new Promise((resolve, reject) => {
+            const openRequest = indexedDB.open(DB_NAME, 1);
+
+            openRequest.onupgradeneeded = () => {
+                openRequest.result.createObjectStore(STORE_NAME);
+            };
+
+            openRequest.onsuccess = () => {
+                const database = openRequest.result;
+                database.onversionchange = () => {
+                    database.close();
+                    databasePromise = null;
+                };
+                resolve(database);
+            };
+
+            openRequest.onerror = () => reject(openRequest.error);
+        });
+    }
+    return databasePromise;
 }
 
-async function getStore(mode: IDBTransactionMode = "readwrite"): Promise<IDBObjectStore> {
-    if (!interscopeMapDB) await initDB();
-    return interscopeMapDB.transaction(storeName, mode).objectStore(storeName);
+async function performStoreOperation<T>(
+    mode: IDBTransactionMode,
+    requestFactory: (store: IDBObjectStore) => IDBRequest<T>
+): Promise<T> {
+    const dbRequest = async (): Promise<T> => {
+        const database = await openDatabase();
+        const transaction = database.transaction(STORE_NAME, mode);
+        const objectStore = transaction.objectStore(STORE_NAME);
+        return promisifyRequest(requestFactory(objectStore));
+    }
+
+    try {
+        return await dbRequest();
+    } catch {
+        // TODO: Might need to be handled in better way
+        databasePromise = null;
+        return await dbRequest();
+    }
 }
 
 export const InterscopeMap = {
-    set: async (key: IDBValidKey, value: any): Promise<IDBValidKey> => wrapRequest((await getStore()).put(value, key)),
-    get: async (key: IDBValidKey): Promise<any> => wrapRequest((await getStore("readonly")).get(key)),
-    delete: async (key: IDBValidKey): Promise<undefined> => wrapRequest((await getStore()).delete(key)),
-    clear: async (): Promise<undefined> => wrapRequest((await getStore()).clear())
+    set: (key: IDBValidKey, value: any) =>
+        performStoreOperation("readwrite", store => store.put(value, key)),
+
+    get: (key: IDBValidKey) =>
+        performStoreOperation("readonly",  store => store.get(key)),
+
+    delete: (key: IDBValidKey) =>
+        performStoreOperation("readwrite", store => store.delete(key)),
+
+    clear: () =>
+        performStoreOperation("readwrite", store => store.clear()),
 };
