5c39aec97f264ff82682ffe30aae311bfde42347
Veli <veli.omn@gmail.com>
2025-08-04 19:54:25 +0200

    fix: copy dir exclude logic

--- node/copy-dir.ts
+++ node/copy-dir.ts
@@ -2,34 +2,33 @@ import fs from "fs";
 import path from "path";
 import { copyFile } from "./copy-file.js";
 
-
-export async function copyDir(srcDir: string, destDir: string, exclude: Array<string> = [], isRootCall = true): Promise<number> {
+export async function copyDir(srcDir: string, destDir: string, exclude: Array<string> = []): Promise<number> {
     let filesCount: number = 0;
 
     try {
-        const entries = await fs.promises.readdir(srcDir, { withFileTypes: true });
         await fs.promises.mkdir(destDir, { recursive: true }).catch(() => {});
+        const entries = await fs.promises.readdir(srcDir, { withFileTypes: true });
 
         for (const entry of entries) {
             const srcPath: string = path.join(srcDir, entry.name);
             const destPath: string = path.join(destDir, entry.name);
 
             if (entry.isDirectory()) {
-                // Recursive call for directories with isRootCall set to false.
-                const copiedInSubdir: number = await copyDir(srcPath, destPath, exclude, false);
-                filesCount += copiedInSubdir; // Add the count from the subdirectory.
-            } else if (entry.isFile() && !exclude.includes(path.extname(entry.name))) {
-                await copyFile(srcPath, destPath);
-                filesCount++;
+                const copiedInSubdir: number = await copyDir(srcPath, destPath, exclude);
+                filesCount += copiedInSubdir;
+            } else if (entry.isFile()) {
+                const fileExtension: string = path.extname(entry.name);
+                const isExcluded = exclude.includes(entry.name) || (fileExtension && exclude.includes(fileExtension));
+
+                if (!isExcluded) {
+                    await copyFile(srcPath, destPath);
+                    filesCount++;
+                }
             }
         }
     } catch (err) {
-        throw new Error(`Failed to copy directory: ${srcDir}`, { cause: err });
+        throw new Error(`Failed to copy directory from ${srcDir} to ${destDir}`, { cause: err });
     }
 
-    if (isRootCall){
-        return filesCount;
-    }
-
-    return filesCount; // Return the count for use in recursive calls.
+    return filesCount;
 }
